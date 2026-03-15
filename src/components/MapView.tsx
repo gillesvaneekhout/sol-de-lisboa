da9e6ddb250b07ef1e7fc02e320661651dd0f9f8
Gilles Van Eekhout <gillesvaneekhout@Mac.lan>
2026-03-15 18:28:21 +0000

    fix: switch map tiles from Stadia Maps to CARTO dark (no auth required)

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -36,10 +36,11 @@ export default function MapView({
     });
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
       {
         attribution:
-          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: "abcd",
         maxZoom: 19,
       }
     ).addTo(map);
